dc4eed6fdb0834e7aa13a8b1165155669094a503
fonsecarosa <fernando@fnfr.com.br>
2023-09-21 14:21:42 -0300

    app layout using dashly

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="./assets/css/theme.bundle.css" id="stylesheetLTR">
+    <link rel="stylesheet" href="./assets/css/theme.rtl.bundle.css" id="stylesheetRTL">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
+</head>
+
+<body>
+
+    {{ $slot }}
+
+    <!-- JAVASCRIPT-->
+    <!-- Theme JS -->
+    <script src="./assets/js/theme.bundle.js"></script>
+
+</body>
+
+</html>
